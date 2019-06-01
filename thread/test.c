0b9e154ed057d391489a65bda6223e42754e4013
HangHangbo <1679759074@qq.com>
2019-06-01 14:32:56 +0800

    线程练习Day2

--- thread/test.c
+++ thread/test.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/time.h>
 # if 0
 //int arr[1000000]={0};
 void * ThreadEntry(void *arg)
@@ -127,38 +128,96 @@ int main()
 //4.通过多线程提高程序执行效率
 //   假设存在一个很大的数组，把数组中的每一个元素都执行一个成方运算
 //   再赋值回数组中
- 
-#define SIZE 100000000
+#if 0
+//单线程版本
+#define SIZE 100000000 
+int64_t GetUs()     //获取毫秒 
+{ 
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  return tv.tv_sec *1000000+tv.tv_usec;
+} 
 void Calc(int*arr,int beg,int end)
-{
+{ 
   int i=beg;
   for(i;i<end;i++)
-  {
+  { 
     arr[i]=arr[i]*arr[i];
   }
 }
-int main()
+int main() 
 {
-  int *arr=(int*)malloc(sizeof(int*)*SIZE);
-  Calc(arr,0,SIZR);
+  srand(time(NULL));
+  int *arr=(int*)malloc(sizeof(int*)*SIZE);//开始记录时间 
+  int64_t beg=GetUs();
+  Calc(arr,0,SIZE); //记录结束时间 
+  int64_t end=GetUs();
+  //两个时间做差，得到Calc执行时间
+  printf("time => %ld\n",end-beg);
   return 0;
 }
+#endif
 
+#if 1
+//多线程版本
+#define SIZE 100000000
+#define THREAD_NUM 4
+void Calc(int*arr,int beg,int end)
+{ 
+  int i=beg;
+  for(i;i<end;++i)
+  { 
+    arr[i]=arr[i]*arr[i];
+  }
+}
+int64_t GetUs()     //获取毫秒
+{
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  return tv.tv_sec *1000000+tv.tv_usec;
+}
+typedef struct Arg{
+  int beg;
+  int end;
+  int* arr;
+} Arg;
+void* ThreadEntry(void *arg)
+{
+  Arg*p=(Arg*)arg;
+  Calc(p->arr,p->beg,p->end);
+  return NULL;
+}
+//线程1 Calc（arr，0，size/2);
+//线程2 Calc (arr,size/2,size);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int main()
+{
+  srand(time(NULL));
+  int* arr=(int*)malloc(sizeof(int)*SIZE);
+  pthread_t tid[THREAD_NUM];
+  Arg args[THREAD_NUM];
+  int base=0;
+  int i=0;
+  for(i=0;i<THREAD_NUM;i++)
+  {
+    args[i].beg=base;
+    args[i].end=base+SIZE/THREAD_NUM;
+    args[i].arr=arr;
+    base+=SIZE/THREAD_NUM;
+  }
+  int64_t beg=GetUs();
+  int b=0;
+  for(b=0;b<THREAD_NUM;++b)
+  {
+    pthread_create(&tid[b],NULL,ThreadEntry,&args[b]);
+  }
+  int a=0;
+  for(a=0;a<THREAD_NUM;++a)
+  {
+    pthread_join(tid[a],NULL);
+  }
+ int64_t end=GetUs();
+ printf("time=>%ld\n",end-beg);
+  return 0;
+}
+#endif
