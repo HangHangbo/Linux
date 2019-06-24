8bda8ae3617e1f9bb8583506fbbbe7a359195681
HangHangbo <1679759074@qq.com>
2019-06-24 22:46:33 +0800

    利用sstream

--- tcp_calc/clac_server.cc
+++ tcp_calc/clac_server.cc
@@ -1,11 +1,24 @@
 #include <sstream>
 #include "tcp_server.hpp"
 
+
  int main(){
      TcpThreadServer server;
-     server.Start("0.0.0.0",9090,[](const std::string&req,std::string *resp)
-     {
-         
+     server.Start("0.0.0.0",9090,[](const std::string& req,std::string *resp){
+         std::stringstream ss;
+         int i,j;
+         char flog;
+         ss<<req;
+         ss>>i;
+         ss.clear();
+         ss<<req;
+         ss>>j;
+         ss.clear();
+         ss<<req;
+         ss>>flog;
+         if(flog=='+'){
+             (*resp).push_back(i+j);
+         }
      });
      return 0;
  }
